4a26b016168b3e13cb9ee05998797689ee737aaa
Hakan Soysal <hakan.soysal@aurorabilisim.com>
2019-10-09 16:37:19 +0300

    Çıktıyı OpenShift e göndermek için bir dockerfile oluşturuldu.

--- app/Startup.cs
+++ app/Startup.cs
@@ -116,7 +116,7 @@ namespace dotnetcore_keycloak
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
