211a93371e9359fd9a44b7f58b8117995e716d71
Hakan Soysal <hakan.soysal@aurorabilisim.com>
2019-10-14 14:46:44 +0300

    bir container başka bir container a bağlanmaya çalışırken hata alınmasının kontrolü için kontrol tetiklemesi eklendi.

---
+++ app/Controllers/AccessCheckController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_keycloak.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccessCheckController : Controller
+    {
+        [HttpGet("[action]")]
+        public async Task<string> KeyCloak(string url)
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync("http://keycloak-passport.app.aurorabilisim.com/auth/");
+        }
+
+        [HttpGet("[action]")]
+        public async Task<string> Google(string url)
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync("http://google.com");
+        }
+    }
+}
